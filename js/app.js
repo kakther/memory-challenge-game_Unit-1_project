4eadf70ea32a8ba3311c395f7bc8c4a775293557
Kausar Akther <kausar.akther@gmail.com>
2021-03-07 14:24:25 -0500

    add element references

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// --------------------------Variable--------------------------------
+let frontDek
+let backDek
+// --------------------------Chached Element reference---------------
+const cards = document.querySelectorAll('.memory-card');
+const frontFace = document.getElementsByClassName('.front-face');
+const backFace = document.getElementsByClassName('.back-face');
+const flipBtn = document.getElementById('#btn');
+// --------------------------Event Listeners-------------------------
+// --------------------------Functions-------------------------------
+
+
+
+
+
+
+
